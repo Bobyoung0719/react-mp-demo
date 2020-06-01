d602b19038f9a80c2a7939fc7d8961e4be0525c7
Bobyoung0719 <Bobyoung0719@gmail.com>
2020-06-01 14:17:31 +0800

    update babelrc

--- src/main/index.js
+++ src/main/index.js
@@ -1,49 +1,31 @@
 import ReactDOM from 'react-dom';
-import React, {Component, lazy, Suspense} from 'react';
+import React, {useState, lazy, Suspense} from 'react';
 import HOC from '$com/HOC';
 import Test from '$com/Test';
 import {getRealUrl} from 'utils/tool';
 import styles from './main.scss';
 
+function Main() {
+  const [Com, setCom] = useState(null);
 
-
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Com: null
-    };
-
-    this.onAction = this.onAction.bind(this);
-  }
-
-  componentDidMount() {
-    
-  }
-
-  onAction() {
+  function onAction() {
     const A = lazy(() => import('$com/Test'));
 
     console.log(A, 'AAAAAAAAÃ');
-    this.setState({Com: A});
+    setCom(A);
   }
-
-  render() {
-
-    const {Com} = this.state;
-
-    return (
-      <div className={styles.container}>
-        首页---
-        <Test />
-        <Suspense fallback={<div>oooo</div>}>
+    
+  return (
+    <div className={styles.container}>
+      首页---
+      <Test />
+      <Suspense fallback={<div>oooo</div>}>
         {Com && <Com />}
-        </Suspense>
-        <button onClick={this.onAction}>onClick</button>
-        <p onClick={() => getRealUrl('page')}>跳转第二页</p>
-      </div>
-    );
-  }
+      </Suspense>
+      <button onClick={onAction}>onClick</button>
+      <p onClick={() => getRealUrl('page')}>跳转第二页</p>
+    </div>
+  );
 }
 
 const HoComponent = HOC(Main);
