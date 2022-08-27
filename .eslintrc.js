a3eae15e2c1215522fa4619ba2d4f7f0553c0e4c
杨涛 <bobyoung0719@gmail.com>
2022-08-27 15:21:06 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'no-extra-semi': 'off',
+    indent: ['error', 2]
+  }
+};
--- src/main/index.js
+++ src/main/index.js
@@ -7,21 +7,12 @@ import sty from './main.scss';
 function Main(props) {
   useEffect(() => {
     console.log(performance);
-    console.log('objectsww111111111qq111')
-
-    // "browserslist": [
-      //   "defaults",
-      //   "not ie < 11",
-      //   "last 2 versions",
-      //   "> 1%",
-      //   "iOS 7",
-      //   "last 3 iOS versions"
-      // ],
+    console.log('eeee');
   }, []);
 
   return (
     <div className={sty.container}>
-      首页-
+      首页-
       <p onClick={() => getRealUrl('page')}>跳转第二页</p>
     </div>
   );
